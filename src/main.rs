2ba14f7fc619058a42ff5288536da38eccad12f5
Andrea Nanni <andrea.nanni@ditechspa.it>
2023-03-07 12:25:21 +0100

    feat: case insensitive option, color output

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use anyhow::{Context, Result};
 use clap::Parser;
+use colored::*;
 use std::fs::File;
 use std::io::BufRead;
 use std::io::BufReader;
@@ -7,31 +8,44 @@ use std::path::PathBuf;
 
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(Debug, Parser)]
+#[command(author, version, about, long_about = None)]
 struct Cli {
     /// The pattern to look for
     pattern: String,
     /// The path to the file to read
     path: PathBuf,
+    /// Case insensitive matching
+    #[arg(short, long)]
+    ignore_case: bool,
 }
 
 fn main() -> Result<()> {
-    let Cli { pattern, path } = Cli::parse();
+    let Cli {
+        pattern,
+        path,
+        ignore_case,
+    } = Cli::parse();
 
     let file = open_file(&path)?;
 
-    find_matches(&file, &pattern)
+    find_matches(&file, &pattern, ignore_case)
 }
 
 fn open_file(path: &PathBuf) -> Result<File> {
-    File::open(path).with_context(|| format!("could not read file `{}`", path.to_str().unwrap()))
+    File::open(path).with_context(|| format!("Could not read file `{}`", path.to_str().unwrap()))
 }
 
-fn find_matches(file: &File, pattern: &str) -> Result<()> {
+fn find_matches(file: &File, pattern: &str, ignore_case: bool) -> Result<()> {
     let reader = BufReader::new(file);
     for (index, line) in reader.lines().enumerate() {
-        let curr = line?;
-        if curr.contains(pattern) {
-            println!("line {} - {}", index + 1, curr.trim());
+        let curr = line.with_context(|| format!("Could not read line `{}`", index))?;
+
+        if (ignore_case && curr.to_lowercase().contains(&pattern.to_lowercase()))
+            || curr.contains(&pattern)
+        {
+            // let line = curr.replace(pattern, pattern.red().bold().to_string().as_str());
+            let line_num = (index + 1).to_string().dimmed();
+            println!("{: >3} {}", line_num, curr);
         }
     }
     Ok(())
